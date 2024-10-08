e4950f00cfe15973337f34cb9f458d557a8224f0
Akashkumar S <s.akashkumar004@gmail.com>
2024-10-08 22:25:45 +0530

    Update api.js

--- src/ngo/api.js
+++ src/ngo/api.js
@@ -4,6 +4,7 @@ const getProjects =()=>axios.get(`${API}/ngo/all`)
 const getProjects1=()=>axios.get(`${API}/ngo1/all`)
 const getProjects2=()=>axios.get(`${API}/ngo2/all`)
 const getProjects3=()=>axios.get(`${API}/blog/all`)
+const getProjects4=()=>axios.get(`${API}/donation/all`)
 const getProjectsbyID=(id)=>axios.get(`${API}/ngo/${id}`)
 const getProjectsbyID1=(id)=>axios.get(`${API}/ngo1/${id}`)
 const getProjectsbyID2=(id)=>axios.get(`${API}/ngo2/${id}`)
@@ -12,5 +13,6 @@ const addProjects=(projectdata)=>axios.post(`${API}/ngo/add`,projectdata)
 const addProjects1=(projectdata)=>axios.post(`${API}/ngo1/add`,projectdata)
 const addProjects2=(projectdata)=>axios.post(`${API}/ngo2/add`,projectdata)
 const addProjects3=(projectdata)=>axios.post(`${API}/blog/add`,projectdata)
+const addProjects4=(projectdata)=>axios.post(`${API}/donation/add`,projectdata)
 
-export { getProjects,getProjects1,getProjects2,getProjectsbyID,getProjectsbyID1,getProjectsbyID2,addProjects,addProjects1,addProjects2,addProjects3,getProjects3,getProjectsbyID3};
+export { getProjects,getProjects1,getProjects2,getProjectsbyID,getProjectsbyID1,getProjectsbyID2,addProjects,addProjects1,addProjects2,addProjects3,addProjects4,getProjects3,getProjects4,getProjectsbyID3};
